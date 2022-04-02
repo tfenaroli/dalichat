2d574879e5c453155bcef3f69085d9989d3e69c9
Thomas Fenaroli <tfenaroli@icloud.com>
2022-04-02 18:43:05 -0400

    changed header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
     return (
         <div>
-            <h1 className="mt-3">Social Media App</h1>
+            <h1 className="mt-3">DaliGram</h1>
         </div>
     );
 }
